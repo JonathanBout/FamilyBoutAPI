df40ea422a9ccaf21c851dd07a0dcb5dbcb5ee82
JonathanBout <jonathanbout01@gmail.com>
2022-07-07 12:55:14 +0200

    update 1

--- FamilyBoutAPI/Program.cs
+++ FamilyBoutAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using System.Net;
+
 namespace FamilyBoutAPI
 {
     public class Program
@@ -9,19 +12,38 @@ namespace FamilyBoutAPI
             // Add services to the container.
             builder.Services.AddAuthorization();
 
+            builder.Services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.KnownProxies.Add(IPAddress.Parse("10.0.0.100"));
+            });
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
 
-            app.UseHttpsRedirection();
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseHttpsRedirection();
+            }
 
             app.UseAuthorization();
+            
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            app.UseAuthentication();
 
             app.MapGet("/hehe", (string? x) =>
             {
-                return "Up: " + x?.ToUpper() + " Low: " + x?.ToLower();
+                if (x is string value)
+                {
+                    return "Up: " + value?.ToUpper() + " Low: " + value?.ToLower();
+                }
+                return "Hehe :)";
             });
+
 
             app.Run();
         }
