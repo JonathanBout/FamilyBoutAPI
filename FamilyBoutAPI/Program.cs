9d6d19b06a74f76bd4c9e3997e395e9c10ab776e
JonathanBout <jonathanbout01@gmail.com>
2022-07-07 14:51:23 +0200

    fix 6

--- FamilyBoutAPI/Program.cs
+++ FamilyBoutAPI/Program.cs
@@ -30,7 +30,7 @@ namespace FamilyBoutAPI
             
             app.UseForwardedHeaders(new ForwardedHeadersOptions
             {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
             });
 
             //app.UseAuthentication();
